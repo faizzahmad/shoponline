2696a96f87448e637d5e58b512925c2201be4557
faiz ahmad <faizahmedkgn85@gmail.com>
2025-05-23 11:05:52 +0530

    resolevd shop page build error

--- src/app/(site)/shop/page.tsx
+++ src/app/(site)/shop/page.tsx
@@ -2,18 +2,25 @@ import { getLatestCategories } from "@/actions/category";
 import { ShopSidebar } from "./_components/shop-sidebar";
 import { ShopPage } from "./_components/shop-page";
 
+interface SubCategory {
+  _id: string;
+  title: string;
+  image: string;
+}
+
+
 const Shop = async() => {
  const categories = await getLatestCategories(50);
- const categoriesData = categories.map((category) => ({
-    id: category._id.toString(),
-    name: category.title,
-    img: category.image,
-    subCategories: category.subCategories?.map((sub: any) => ({
+const categoriesData = categories.map((category) => ({
+  id: category._id.toString(),
+  name: category.title,
+  img: category.image,
+  subCategories: category.subCategories?.map((sub: SubCategory) => ({
     id: sub._id.toString(),
     name: sub.title,
     img: sub.image,
   })) || [],
-  }));
+}));
     return ( 
         <div className="w-full flex gap-5">
            <div className="w-64 h-screen border-r p-5 overflow-y-auto">
