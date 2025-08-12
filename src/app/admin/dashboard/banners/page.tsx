f059287a241aeb1f381f3da5c40e23947926744c
faiz ahmad <faizahmedkgn85@gmail.com>
2025-08-12 18:16:14 +0530

    resolved all build issue

--- src/app/admin/dashboard/banners/page.tsx
+++ src/app/admin/dashboard/banners/page.tsx
@@ -1,9 +1,13 @@
 import { BannerComponent } from "./_components/banner-component";
+import { Suspense } from "react";
 
 const Banner = () => {
     return ( 
         <div className="w-full h-screen overflow-y-auto bg-gray-50">
+             <Suspense fallback={<p>Loading data...</p>}>
         <BannerComponent/>
+      </Suspense>
+      
         </div>
      );
 }
--- src/app/admin/dashboard/products/page.tsx
+++ src/app/admin/dashboard/products/page.tsx
@@ -1,11 +1,14 @@
 
 import { ProductHome } from "./_components/product-home";
+import { Suspense } from "react";
 
 const DashboardProducts = async() => {
    
     return (
         <div className="w-full p-5 bg-gray-50">
+            <Suspense fallback={<p>Loading products...</p>}>
             <ProductHome/>
+            </Suspense>
         </div>
       );
 }
